9f7ebdcaf73cf3ca000e01cce34209a600177dbb
hendrijanuri <hendrijanuri@gmail.com>
2020-12-05 15:39:40 +0700

    bool

---
+++ 01Basics/nullandbool.js
@@ -0,0 +1,20 @@
+// temp is coming from website API
+
+var temp = 0
+
+console.log('Current temperature: ' + temp);
+
+// Grade system
+
+// 10 - great job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+//boolean value = true/false
+
+let actualMarks = 7
+
+let myGrade = (actualMarks == 10)
+
+console.log(myGrade);
